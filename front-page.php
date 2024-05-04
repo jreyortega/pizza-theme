971d33f4774917610a44c06847db1b9b840800bd
anieva03 <anival03@gmail.com>
2024-05-04 19:14:59 +0200

    .
    .

---
+++ front-page.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Front Page
+*/
+?>
+<?php get_header('front');?>
+
+<section class="welcome container">
+    <h2><?php the_field('title_welcome'); ?></h2>
+    <p><?php the_field('content_welcome'); ?></p>
+</section>
+
+<section class="main-games">
+    <div class="container">
+        <h2>Our games</h2>
+        <?php vistapizza_list_pizzas(4); ?>
+        <div class="button-container">
+            <a href="<?php echo esc_url(get_permalink(get_page_by_title('Pizzas'))); ?>" class="button">See All</a>
+        </div>
+    </div>
+</section>
+
+<section class="sellers">
+    <div class="container">
+        <h2>Our sellers</h2>
+        <?php vistapizza_list_employees(10); ?>
+    </div>
+</section>
+
+
+<section class="countdown">
+    <p id="date-countdown">
+        <?php the_field('date_countdown'); ?>
+    </p>
+    <div class="container">
+        <h2>Next conference</h2>
+        <div class="countdown-numbers">
+            <div class="days">
+                <p class="number" id="days"></p>
+                <p class="countdown-tag">days</p>
+            </div>
+            <div class="hours">
+                <p class="number" id="hours"></p>
+                <p class="countdown-tag">hours</p>
+            </div>
+            <div class="minutes">
+                <p class="number" id="minutes"></p>
+                <p class="countdown-tag">minutes</p>
+            </div>
+            <div class="seconds">
+                <p class="number" id="seconds"></p>
+                <p class="countdown-tag">seconds</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ js/script.js
@@ -0,0 +1,40 @@
+(function(){
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function(){
+    try{
+        let date_countdown = document.getElementById('date-countdown').innerText;
+        let d = date_countdown.split('-');
+        var countDownDate = new Date(d[0], d[1], d[2], d[3], d[4], d[5]).getTime();
+        var x = setInterval(downcounter, 1000);
+      }
+      catch(e) {
+        console.error('Error parsing date:', e);
+      }
+  
+      function downcounter(){
+        // Get today's date and time
+        var now = new Date().getTime();
+  
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+  
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+        // Display the result in the element with id="demo"
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+  
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+        }
+      }
+  
+    });
+})();
