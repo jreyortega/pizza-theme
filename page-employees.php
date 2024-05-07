be802e47b7e3ee6f15b0cab91ff795db3b9d4472
José del Rey <jreyortega@al.uloyola.es>
2024-05-07 20:50:51 +0200

    dasdadsa

--- page-employees.php
+++ page-employees.php
@@ -20,7 +20,7 @@ Template Name: Employees
                 the_post();/*initialise WordPress Loop*/ ?>
                 <h1><?php the_title();/*Title of your page */ ?></h1>
                 <?php the_post_thumbnail('blog', array('class' => 'image-class'));/*Image*/ ?>
-                <p><?php /*Content of your page*/ ?></p>
+                <p><?php the_content() ?></p>
             <?php } ?>
             <?php vistapizza_list_employees(10); ?>
         </div>
